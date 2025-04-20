ba93fb325f9ad74d479e8bdbbff303c92666f09b
Al Bowers <abowers822@verizon.net>
2025-04-20 10:22:32 -0400

    Update export.php

--- export.php
+++ export.php
@@ -2,14 +2,17 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
     // Collect form data 
     $name = $_POST['name']; 
-    $email = $_POST['email']; 
+    $sys = $_POST['sys']; 
+    $dia = $_POST['dia'];
+    $pul = $_POST['pul'];
  
     // Create a CSV file 
     $filename = "data.csv"; 
     $file = fopen($filename, "a"); // Open the file in append mode 
- 
     // Prepare the data to be written 
-    $data = [$name, $email]; 
+
+    
+    $data = [$name, $sys, $dia, $pul]; 
  
     // Write the data to the CSV file 
     fputcsv($file, $data); 
