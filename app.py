e3714d2c7e4187af540b0c5d72fa3c4135fee2d9
Al Bowers <abowers822@verizon.net>
2025-04-20 20:54:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 	"<h1><center>Hello, World!</center></h1>"
+	return 	"<h1><center>Hello, World!</center></h1>"
     		"<h3> <figure><img  src="sunwave16.bmp""
             "alt="SWS" width="60" height="75">"
             "<figcaption>SunWave Software</figcaption>"
