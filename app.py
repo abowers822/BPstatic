d1070c0e7f9c7993a7afb038ba71b717cc9e16a3
Al Bowers <abowers822@verizon.net>
2025-04-20 20:45:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
     return "<h1><center>Hello, World!</center></h1>"
     	    "<h3> <figure><img  src="sunwave16.bmp""
             "alt="SWS" width="60" height="75">"
-            "<figcaption>SunWave Software</figcaption>
+            "<figcaption>SunWave Software</figcaption>"
             " </figure> </h3"
